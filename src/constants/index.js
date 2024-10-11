940badac03508dbdbeb30aede2efa78581faaeac
SHUBHAM KUMAR <117589831+SHUBHAMKUMAR45@users.noreply.github.com>
2024-10-11 15:43:09 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,198 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Emily Johnson',
+    position: 'Marketing Director at GreenLeaf',
+    img: 'assets/review1.png',
+    review:
+      'Working with Shubham was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+  },
+  {
+    id: 2,
+    name: 'Mark Rogers',
+    position: 'Founder of TechGear Shop',
+    img: 'assets/review2.png',
+    review:
+      'Shubham’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+  },
+  {
+    id: 3,
+    name: 'John Dohsas',
+    position: 'Project Manager at UrbanTech ',
+    img: 'assets/review3.png',
+    review:
+      'I can’t say enough good things about Shubham. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+  },
+  {
+    id: 4,
+    name: 'Ether Smith',
+    position: 'CEO of BrightStar Enterprises',
+    img: 'assets/review4.png',
+    review:
+      'Shubham was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'Ecomzy ',
+    desc: 'A modern, streamlined shopping bag application built with React.js, Redux Toolkit, and Tailwind CSS. React handles the component-based structure and smooth UI interactions. Redux Toolkit streamlines your state management, making it easy to keep track of items in your shopping bag, update quantities, and handle checkout processes. Tailwind CSS ensures your app looks sleek and responsive without spending hours on custom styling . ',
+    subdesc:
+      'Built  dynamic, visually appealing shopping experience where users can effortlessly browse, add, and remove items in their shopping bag .',
+    href: 'https://shopping-bag-livid.vercel.app/',
+    texture: '/textures/project/ecomzyy.mp4',
+    logo: '/assets/logo.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+    ],
+  },
+  {
+    title: 'Thought Saver',
+    desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
+    subdesc:
+      'Utilize CSS in your resume to maintain consistency, enhance readability, and create a visually appealing layout while staying true to professional design principles.',
+    href: 'https://node-js-project-l0yo.onrender.com/',
+    texture: '/textures/project/tought.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'EJS',
+        path: '/assets/ejs.png',
+      },
+      {
+        id: 2,
+        name: 'Node Js',
+        path: 'assets/nodejs-icon.webp',
+      },
+    ],
+  },
+  {
+    title: 'Obys Agency',
+    desc: ' Implemented modern web development techniques to create a vibrant and interactive user experience .',
+    subdesc:
+      ' Demonstrated technical expertise and creativity in web development through the project.',
+    href: 'https://shubhamkumar45.github.io/OBYS-AGENCY/',
+    texture: '/textures/project/obye.mp4',
+    logo: '/assets/obys.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'Gsap',
+        path: '/assets/gsap.jpg',
+      },
+    ],
+  },
+  {
+    title: 'Document Wallet',
+    desc: 'Node.js is your server-side backbone, ensuring scalable, high-performance handling of requests and operations.Express.js simplifies your server-side logic and routing, making it easy to manage HTTP requests and build out the API endpoints for your app.',
+    subdesc:
+      'EJS (Embedded JavaScript) is your templating engine, which dynamically generates HTML with minimal fuss,keeping your front-end and back-end closely connected',
+    href: 'https://doc-wallets.onrender.com/',
+    texture: '/textures/project/docsaveer.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'Node js',
+        path: 'assets/nodejs-icon.webp',
+      },
+      {
+        id: 2,
+        name: 'EJS',
+        path: '/assets/ejs.png',
+      },
+    ],
+  },
+  {
+    title: 'Counter App',
+    desc: ' This app allows users to increase, decrease, and reset a counter.',
+    subdesc:
+      'Reacts component-based structure makes it easy to manage the state and handle user interactions smoothly. The interface is intuitive and responsive, thanks to Tailwind CSS, ensuring a seamless user experience.',
+    href: 'https://react-counter-app-lake.vercel.app/',
+    texture: '/textures/project/reactcounter.mp4',
+    logo: '/assets/project-logo5.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,32 @@
+const Footer = () => {
+  return (
+    <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
+      <div className="text-white-500 flex gap-2">
+        <p>Terms & Conditions</p>
+        <p>|</p>
+        <p>Privacy Policy</p>
+      </div>
+
+      <div className="flex gap-3">
+
+      <div className="social-icon">
+          <a href="https://www.linkedin.com/in/-shubham-kumar/" target="_blank" rel="noopener noreferrer">
+          <img src="/assets/linked.svg" alt="linkedin" className="w-5 h-1/2" /></a>
+        </div>
+        <div className="social-icon">
+          <a href="https://github.com/SHUBHAMKUMAR45" target="_blank" rel="noopener noreferrer">
+          <img src="/assets/github.svg" alt="github" className="w-5 h-1/2" /></a>
+        </div>
+        <div className="social-icon">
+          <a href="https://www.instagram.com/i_am__shubham_kumar/">
+          <img src="/assets/instagram.svg" alt="instagram" className="w-5 h-1/2"  />
+          </a>
+        </div>
+      </div>
+
+      <p className="text-white-500">© 2024 SHUBHAM KUAMR . All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
